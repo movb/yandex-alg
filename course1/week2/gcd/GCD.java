ec33b03facad2f02773f00bbdf3915f9720128d5
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2016-03-08 14:20:38 +0300

    Alg1: Greatest common divisor.

---
+++ course1/week2/gcd/GCD.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class GCD {
+  private static int gcd(int a, int b) {
+    if (b==0)
+        return a;
+    else {
+        if (b>a)
+            return gcd(b,a);
+        else
+            return gcd(b, a % b);
+    }
+  }
+
+  public static void main(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+
+    System.out.println(gcd(a, b));
+  }
+}
