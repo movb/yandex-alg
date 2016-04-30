15711a977b4ee56803913f70fec108125eb79a85
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2016-04-30 22:14:29 +0300

    Alg1: Week3 Covering Segments problem solution.

--- course1/week3/covering_segments/CoveringSegments.java
+++ course1/week3/covering_segments/CoveringSegments.java
@@ -3,21 +3,37 @@ import java.util.*;
 public class CoveringSegments {
 
     private static int[] optimalPoints(Segment[] segments) {
-        //write your code here
-        int[] points = new int[2 * segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
+        int[] points = new int[segments.length];
+        
+        Arrays.sort(segments);
+        int cur_index = 0;
+        int max_point = Integer.MIN_VALUE;
+        
+        for(Segment segment: segments) {
+            if(segment.start > max_point) {
+                points[cur_index] = segment.end;
+                max_point = segment.end;
+                ++cur_index;
+            }
         }
-        return points;
+        int[] new_points = new int[cur_index];
+        System.arraycopy(points, 0, new_points, 0, cur_index);
+        
+        return new_points;
     }
 
-    private static class Segment {
+    private static class Segment implements Comparable {
         int start, end;
 
         Segment(int start, int end) {
             this.start = start;
             this.end = end;
+        }
+        
+        public int compareTo(Object other)
+        {
+            Segment o = (Segment)other;
+            return(end - o.end);
         }
     }
     public static void main(String[] args) {
