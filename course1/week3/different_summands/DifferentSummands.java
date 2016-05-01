16cb09fb583309a99a05d470421a182e0aee5df8
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2016-05-01 19:10:37 +0300

    Alg1: Week3 Different Summands problem solution.

--- course1/week3/different_summands/DifferentSummands.java
+++ course1/week3/different_summands/DifferentSummands.java
@@ -3,7 +3,16 @@ import java.util.*;
 public class DifferentSummands {
     private static List<Integer> optimalSummands(int n) {
         List<Integer> summands = new ArrayList<Integer>();
-        //write your code here
+        
+        int max_k = (int)Math.floor((Math.sqrt(1+8*(long)n) - 1)/2);
+        int sum = 0;
+        for(int i=1;i<max_k;++i) {
+            summands.add(i);
+            sum+=i;
+        }
+        
+        summands.add(n-sum);
+        
         return summands;
     }
     
