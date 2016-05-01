7b4414785c01ebe814253e9c5e5fa0c36f025733
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2016-05-01 19:37:58 +0300

    Alg1: Week3 Largest Number partial solution.

--- course1/week3/largest_number/LargestNumber.java
+++ course1/week3/largest_number/LargestNumber.java
@@ -1,8 +1,28 @@
 import java.util.*;
 
 public class LargestNumber {
+    static class StrComparator implements Comparator<String>
+    {
+        public int compare(String s1, String s2)
+        {
+            int min_len = Math.min(s1.length(), s2.length());
+            for(int i=0;i<min_len;++i) {
+                if(s1.charAt(i)<s2.charAt(i))
+                    return 1;
+                else if(s2.charAt(i)<s1.charAt(i))
+                    return -1;
+            }
+            if (s1.length()<s2.length())
+                return s2.charAt(min_len) - s2.charAt(min_len-1);
+            else if (s2.length()<s1.length())
+                return s1.charAt(min_len) - s1.charAt(min_len-1);;
+            return 0;
+        }
+    }
+
     private static String largestNumber(String[] a) {
-        //write your code here
+        Arrays.sort(a, new StrComparator());
+        
         String result = "";
         for (int i = 0; i < a.length; i++) {
             result += a[i];
