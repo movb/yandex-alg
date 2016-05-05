decd22b8d50de438480993ee2ae71d9b85b9ae9d
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2016-05-05 23:22:01 +0300

    Alg1: Week5 Primitive Calculator problem solution.

--- course1/week5/primitive_calculator/PrimitiveCalculator.java
+++ course1/week5/primitive_calculator/PrimitiveCalculator.java
@@ -3,16 +3,33 @@ import java.util.*;
 public class PrimitiveCalculator {
     private static List<Integer> optimal_sequence(int n) {
         List<Integer> sequence = new ArrayList<Integer>();
-        while (n >= 1) {
-            sequence.add(n);
-            if (n % 3 == 0) {
-                n /= 3;
-            } else if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n -= 1;
+        int[] a = new int[n];
+        
+        a[0] = 0;
+        for(int i=1;i<=n;++i) {
+            if(i>1)
+                a[i-1] = a[i-2] + 1;
+                
+            if (i % 3 == 0) {
+                int min_steps = a[i/3 - 1] + 1;
+                if (a[i-1] > min_steps) a[i-1] = min_steps;
+            } else if (i % 2 == 0) {
+                int min_steps = a[i/2 - 1] + 1;
+                if (a[i-1] > min_steps) a[i-1] = min_steps;
             }
         }
+        
+        int i=n;
+        while(i>0) {
+            sequence.add(i);
+            int next_i = i-1;
+            if (i%3 == 0 && a[i-1] == (a[i/3 -1] + 1))
+                next_i = i/3;
+            if (i%2 == 0 && a[i-1] == (a[i/2 -1] + 1))
+                next_i = i/2;
+            i = next_i;
+        }
+        
         Collections.reverse(sequence);
         return sequence;
     }
