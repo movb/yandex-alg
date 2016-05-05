4fb56562c71f45c25f5aef45aacce4adcbc4a0aa
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2016-05-06 00:13:45 +0300

    Alg1: Week5 Knapsack problem solution.

--- course1/week5/knapsack/Knapsack.java
+++ course1/week5/knapsack/Knapsack.java
@@ -2,14 +2,27 @@ import java.util.*;
 
 public class Knapsack {
     static int optimalWeight(int W, int[] w) {
-        //write you code here
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
-          }
+        int m[][] = new int[w.length+1][W+1];
+        
+        for(int i=0;i<W+1;++i) {
+            m[0][i] = 0;
         }
-        return result;
+        for(int j=0;j<(w.length+1);++j) {
+            m[j][0] = 0;
+        }
+        
+        for(int i=1;i<w.length+1;++i) {
+            for(int j=1;j<W+1;++j) {
+                m[i][j] = m[i-1][j];
+                if (w[i-1]<=j) {
+                    int val = m[i-1][j-w[i-1]] + w[i-1];
+                    if (m[i][j] < val)
+                        m[i][j] = val;
+                }
+            }
+        }
+        
+        return m[w.length][W];
     }
 
     public static void main(String[] args) {
