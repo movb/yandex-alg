58898801c1f062879c1820b0cb20a04bd8bc602f
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2017-10-24 02:46:35 +0300

    Alg3: Week2 Acyclicity problem

--- course3/week2/acyclicity/acyclicity.cpp
+++ course3/week2/acyclicity/acyclicity.cpp
@@ -4,8 +4,39 @@
 using std::vector;
 using std::pair;
 
+bool explore(vector<vector<int> > &adj, vector<bool> &removed, vector<bool>& visited, int i)
+{
+    visited[i] = true;
+    for(int j = 0; j < adj[i].size(); j++) {
+        if( removed[adj[i][j]] )
+            continue;
+
+        if( visited[adj[i][j]] )
+            return true;
+
+        if(explore(adj, removed, visited, adj[i][j]))
+            return true;
+    }
+    removed[i] = true;
+
+    return false;
+}
+
+
+
 int acyclic(vector<vector<int> > &adj) {
   //write your code here
+  vector<bool> removed(adj.size(), false);
+
+  for(int i=0; i < adj.size(); ++i)
+  {
+      if(removed[i])
+          continue;
+      vector<bool> visited(adj.size(), false);
+      if(explore(adj, removed, visited, i))
+          return 1;
+  }
+
   return 0;
 }
 
