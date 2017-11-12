16debe52e13c38fb57253e043b818225a999467f
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2017-11-13 02:37:19 +0300

    Alg3: Week5 Connecting points problem sotultion

--- course3/week5/connecting_points/connecting_points.cpp
+++ course3/week5/connecting_points/connecting_points.cpp
@@ -3,12 +3,49 @@
 #include <iomanip>
 #include <vector>
 #include <cmath>
+#include <queue>
+#include <limits>
+#include <numeric>
 
 using std::vector;
+using std::priority_queue;
+using std::pair;
+using std::numeric_limits;
+using std::greater;
+
+double dist(int x1, int y1, int x2, int y2) {
+    return sqrt( pow(x1-x2, 2) + pow(y1-y2, 2));
+}
 
 double minimum_distance(vector<int> x, vector<int> y) {
   double result = 0.;
   //write your code here
+  priority_queue<pair<double, int>, vector<pair<double,int>>, greater<pair<double,int>>> q;
+
+  vector<int> discovered(x.size(), 0);
+  vector<double> cost(x.size(), numeric_limits<double>::max());
+
+  q.emplace(0, 0);
+
+  while(!q.empty()) {
+      auto v = q.top();
+      q.pop();
+      //std::cout << "Found " << v.second << " (" << v.first << ")" << std::endl;
+      if (discovered[v.second])
+         continue;
+      discovered[v.second] = 1;
+      cost[v.second] = v.first;
+
+      for(int i=0; i<x.size(); ++i) {
+          if (discovered[i])
+              continue;
+
+          q.emplace(dist(x[v.second], y[v.second], x[i], y[i]), i);
+      }
+  }
+
+  result = std::accumulate(cost.begin(), cost.end(), 0.0);
+
   return result;
 } 
 
