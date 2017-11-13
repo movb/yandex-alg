eaf0fe1e44c5293473e70a2c26960561e927ab31
Mikhail Plekhanov <mike.plekhanov@gmail.com>
2017-11-14 00:32:09 +0300

    Alg2: Week1 Check Brackets problem

--- course2/week1/check_brackets_in_code/check_brackets.cpp
+++ course2/week1/check_brackets_in_code/check_brackets.cpp
@@ -31,15 +31,41 @@ int main() {
         char next = text[position];
 
         if (next == '(' || next == '[' || next == '{') {
-            // Process opening bracket, write your code here
+            opening_brackets_stack.emplace(next, position);
         }
 
         if (next == ')' || next == ']' || next == '}') {
             // Process closing bracket, write your code here
+            if (opening_brackets_stack.empty()) {
+                std::cout << position + 1 << std::endl;
+                return 0;
+            }
+            auto bracket = opening_brackets_stack.top();
+            opening_brackets_stack.pop();
+
+            if(!bracket.Matchc(next)) {
+                std::cout << bracket.position + 1 << std::endl;
+                return 0;
+            }
         }
     }
 
     // Printing answer, write your code here
+    if (!opening_brackets_stack.empty()) {
+        int pos = opening_brackets_stack.top().position;
+        /*
+        int pos = 0;
+        while(!opening_brackets_stack.empty()) {
+            auto bracket = opening_brackets_stack.top();
+            opening_brackets_stack.pop();
+            pos = bracket.position;
+        }
+        */
+        std::cout << pos + 1 << std::endl;
+        return 0;
+    }
+
+    std::cout << "Success" << std::endl;
 
     return 0;
 }
